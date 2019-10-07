5614f6f15696ef7e83028c301a19ff4b82548ab3
Eryk Napierala <eryk.piast@gmail.com>
2019-10-07 21:34:26 +0200

    feat(testing): generate snapshots during tests

--- playground/play.sh
+++ playground/play.sh
@@ -5,13 +5,23 @@ set -e;
 OLD_PWD=$(pwd);
 EXAMPLE=$1;
 
-DIR="$OLD_PWD/playground/$EXAMPLE";
-
-if [[ -d "$DIR" ]]; then
-    cd "$DIR";
-    [[ -d "./node_modules" ]] || npm ci;
-    npm run build;
+if [ -z $EXAMPLE ]; then
+    EXAMPLES=$(find playground -mindepth 1 -maxdepth 1 -type d -exec basename {} \;);
+else
+    EXAMPLES=("$EXAMPLE");
 fi;
+
+for EXAMPLE in ${EXAMPLES[@]}; do
+    echo "Playing with $EXAMPLE";
+
+    DIR="$OLD_PWD/playground/$EXAMPLE";
+
+    if [[ -d "$DIR" ]]; then
+        cd "$DIR";
+        [[ -d "./node_modules" ]] || npm ci;
+        npm run build;
+    fi;
+done
 
 cd "$OLD_PWD";
 
