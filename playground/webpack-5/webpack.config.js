f833d4792127db1c69b8b6bf55270ae19ceee414
Eryk Napierała <eryk.piast@gmail.com>
2020-06-17 00:24:18 +0200

    chore(testing): add webpack 5 playground example

---
+++ playground/webpack-5/webpack.config.js
@@ -0,0 +1,48 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const Plugin = require('webpack-enhanced-stats-plugin');
+
+Plugin.webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                regenerator: true,
+                helpers: false,
+              },
+            ],
+          ],
+        },
+      },
+    }, {
+      loader: Plugin.loader,
+    }],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      sourceMap: true,
+    })],
+  },
+  plugins: [
+    new Plugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+  ],
+};
